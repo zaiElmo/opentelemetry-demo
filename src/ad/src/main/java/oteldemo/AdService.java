9199552eef5b1c63eebc9fcaf385a159c162f00e
zainab <zainabelmoussaoui6@gmail.com>
2025-03-22 12:26:44 +0100

    added failure in AdService

--- src/ad/src/main/java/oteldemo/AdService.java
+++ src/ad/src/main/java/oteldemo/AdService.java
@@ -201,10 +201,8 @@ public final class AdService {
             Attributes.of(
                 adRequestTypeKey, adRequestType.name(), adResponseTypeKey, adResponseType.name()));
 
-        // Throw 1/10 of the time to simulate a failure when the feature flag is enabled
-        if (ffClient.getBooleanValue(AD_FAILURE, false, evaluationContext) && random.nextInt(10) == 0) {
-          throw new StatusRuntimeException(Status.UNAVAILABLE);
-        }
+        // Throw 1/10 of the time to simulate a failure when the feature flag is enabled 
+        throw new StatusRuntimeException(Status.UNAVAILABLE);
 
         if (ffClient.getBooleanValue(AD_MANUAL_GC_FEATURE_FLAG, false, evaluationContext)) {
           logger.warn("Feature Flag " + AD_MANUAL_GC_FEATURE_FLAG + " enabled, performing a manual gc now");
